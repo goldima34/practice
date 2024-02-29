45b42866b6b306c8ab9336a555b05a3050327304
Dima Golovaschuk <golovaschukdima@gmail.com>
2024-02-29 13:28:43 +0200

    Login Form

---
+++ script.js
@@ -0,0 +1,11 @@
+const registerBtn = document.getElementById('register')
+const container = document.getElementById('container')
+const loginBtn = document.getElementById('login')
+
+registerBtn.addEventListener('click', () =>{
+    container.classList.add("active")
+})
+
+login.addEventListener('click', () =>{
+    container.classList.remove("active")
+})
